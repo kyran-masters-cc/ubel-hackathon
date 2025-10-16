f593680cf4b88080cbc56474b145115d268dc02a
jamie-cochrane <jamiecoc@visa.com>
2025-10-16 10:48:40 +0100

    Init spring

---
+++ TransactBench/src/main/java/com/currencycloud/transactbench/TransactBenchApplication.java
@@ -0,0 +1,13 @@
+package com.currencycloud.transactbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactBenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactBenchApplication.class, args);
+    }
+
+}
